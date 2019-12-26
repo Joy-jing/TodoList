f15572f67d062874304cf8744dff6017c3533dff
todo <jing_guoxiang@163.com>
2019-12-26 16:14:10 +0800

    todo

--- src/main/java/cn/neu/jing/controller/TaskController.java
+++ src/main/java/cn/neu/jing/controller/TaskController.java
@@ -2,31 +2,37 @@ package cn.neu.jing.controller;
 
 import cn.neu.jing.entity.Task;
 import cn.neu.jing.service.TaskService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-
+@Controller
+@RequestMapping(path = "/todo")
 public class TaskController {
     private TaskService taskService;
-
+    @RequestMapping(path = "/allTask")
     public String addTask(Task task){
         taskService.addTask(task);
-        return "redirect:/allTask";
+        return "allTask";
     }
     //修改任务
+    @RequestMapping(path="/allTask")
     public String updateTask(Model model,Task task){
         taskService.updateTask(task);
         Task task1 = taskService.selectById(task.getTask_id());
         model.addAttribute("task1",task1);
-        return "/allTask";
+        return "allTask";
     }
     //删除任务
+    @RequestMapping(path="/allTask")
     public String deleteTask(int task_id){
         taskService.deleteTask(task_id);
-        return "/allTask";
+        return "allTask";
 
     }
     //查询全部任务
+    @RequestMapping(path="/allTask")
     public String findAll(Model model){
         List<Task> list = taskService.findAll();
         model.addAttribute("list",list);
@@ -37,25 +43,30 @@ public class TaskController {
         return "";
     }
     //添加今日任务,传入时间？？
+    @RequestMapping(path="/todayTask")
     public String addTodayTask(Task task){
         taskService.addTodayTask(task);
-        return "/todayTask";
+        return "todayTask";
     }
     //添加重要任务
+    @RequestMapping(path="/majorTask")
     public String addMajorTask(Task task){
         taskService.addMajorTask(task);
-        return "/majorTask";
+        return "majorTask";
     }
     //取消今日任务选择
+    @RequestMapping(path="/allTask")
     public String deleteTodayTask(int task_id){
         taskService.deleteTodayTask(task_id);
-        return "/allTask";
+        return "allTask";
 
     }
     //取消重要选择
+    @RequestMapping(path="/allTask")
+
     public String deleteMajorTask(int task_id){
         taskService.deleteMajorTask(task_id);
-        return "/allTask";
+        return "allTask";
 
     }
 }
--- src/main/java/cn/neu/jing/controller/TaskTypeController.java
+++ src/main/java/cn/neu/jing/controller/TaskTypeController.java
@@ -2,31 +2,38 @@ package cn.neu.jing.controller;
 
 import cn.neu.jing.entity.TaskType;
 import cn.neu.jing.service.TaskTypeService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-
+//控制器类
+@Controller
+@RequestMapping(path="/todo")
 public class TaskTypeController {
     private TaskTypeService taskTypeService;
-
+//请求映射，需要提供请求路径
+    @RequestMapping(path="/allTaskType")
     public String addTaskType(TaskType taskType) {
         taskTypeService.addTaskType(taskType);
-        return "/allTaskType";
+        return "allTaskType";
     }
 
     //修改任务类型
+    @RequestMapping(path = "/allTaskType")
     public String updateTaskType(Model model, TaskType taskType) {
         taskTypeService.updateTaskType(taskType);
         TaskType type = taskTypeService.selectById(taskType.getId());
         model.addAttribute("type", type);
-        return "/allTaskType";
+        return "allTaskType";
     }
 
     //查询类型下的任务,需要传递类型？？
+    @RequestMapping(path="/taskType")
     public String selectTaskType(Model model, int type_id) {
         List<TaskType> list = taskTypeService.selectTaskType(type_id);
         model.addAttribute("list",list);
-        return "/taskType";
+        return "taskType";
     }
 
 
